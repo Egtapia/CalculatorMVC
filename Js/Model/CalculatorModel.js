69d45605415f5b2902f57cc4524200b9f80207d2
lupe <egtapiacampos@gmail.com>
2026-09-20 13:57:42 -0600

    Initial calculator project

---
+++ Js/Model/CalculatorModel.js
@@ -0,0 +1,43 @@
+export class CalculatorModel {
+
+    add(a, b) {
+        return a + b;
+    }
+
+    subtract(a, b) {
+        return a - b;
+    }
+
+    multiply(a, b) {
+        return a * b;
+    }
+
+    divide(a, b) {
+        if (b === 0) {
+            throw new Error("Cannot divide by zero");
+        }
+
+        return a / b;
+    }
+
+    calculate(a, b, operator) {
+        switch (operator) {
+            case "+":
+                return this.add(a, b);
+
+            case "-":
+                return this.subtract(a, b);
+
+            case "×":
+            case "*":
+                return this.multiply(a, b);
+
+            case "÷":
+            case "/":
+                return this.divide(a, b);
+
+            default:
+                throw new Error("Invalid operator");
+        }
+    }
+}
